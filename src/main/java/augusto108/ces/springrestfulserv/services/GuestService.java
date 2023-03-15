b896f60aeec50e89f214b30597333fe8f5fb1e17
César Augusto <ces.augusto108@gmail.com>
2023-03-15 15:03:03 -0300

    Guest service implementation CRUD methods and custom exception

--- src/main/java/augusto108/ces/springrestfulserv/services/GuestService.java
+++ src/main/java/augusto108/ces/springrestfulserv/services/GuestService.java
@@ -1,7 +1,15 @@
 package augusto108.ces.springrestfulserv.services;
 
+import java.util.List;
+
 import augusto108.ces.springrestfulserv.model.Guest;
 
 public interface GuestService {
     Guest saveGuest(Guest guest);
+
+    Guest fetchGuest(Long id);
+
+    List<Guest> fetchGuests();
+
+    void deleteGuest(Long id);
 }
--- src/main/java/augusto108/ces/springrestfulserv/services/GuestServiceImpl.java
+++ src/main/java/augusto108/ces/springrestfulserv/services/GuestServiceImpl.java
@@ -1,7 +1,12 @@
 package augusto108.ces.springrestfulserv.services;
 
+import augusto108.ces.springrestfulserv.exceptions.GuestNotFoundException;
 import augusto108.ces.springrestfulserv.model.Guest;
 import augusto108.ces.springrestfulserv.repositories.GuestRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,5 +20,34 @@ public class GuestServiceImpl implements GuestService {
     @Override
     public Guest saveGuest(Guest guest) {
         return repository.save(guest);
+    }
+
+    @Override
+    public Guest fetchGuest(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new GuestNotFoundException("Guest not found. Id: " + id));
+    }
+
+    @Override
+    public List<Guest> fetchGuests() {
+        Iterable<Guest> guests = repository.findAll();
+
+        List<Guest> guestList = new ArrayList<>();
+
+        for (Guest guest : guests) {
+            guestList.add(guest);
+        }
+
+        return guestList;
+    }
+
+    @Override
+    public void deleteGuest(Long id) {
+        Guest guest = fetchGuest(id);
+
+        if (guest == null)
+            throw new GuestNotFoundException("Guest not found. Id: " + id);
+        else
+            repository.delete(guest);
     }
 }

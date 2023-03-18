c48387293ccd9669690590cfd5f200e58ebc88b5
cesaugusto108 <ces.augusto108@gmail.com>
2023-03-18 10:32:20 -0300

    Exception handler shows ResponseEntity of type Problem with 404 Not found message

--- src/main/java/augusto108/ces/springrestfulserv/controllers/handlers/GuestNotFoundHandler.java
+++ src/main/java/augusto108/ces/springrestfulserv/controllers/handlers/GuestNotFoundHandler.java
@@ -1,6 +1,10 @@
 package augusto108.ces.springrestfulserv.controllers.handlers;
 
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mediatype.problem.Problem;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,7 +17,10 @@ public class GuestNotFoundHandler {
     @ExceptionHandler
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleException(GuestNotFoundException e) {
-        return e.getMessage();
+    public ResponseEntity<Problem> handleException(GuestNotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body(Problem.create().withTitle("404 Not found").withDetail(e.getMessage()));
     }
 }

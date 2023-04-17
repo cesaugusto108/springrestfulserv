ae388cb0573d990d323438e6b1245529c0ae8092
César Augusto <ces.augusto108@gmail.com>
2023-04-16 23:49:11 -0300

    Basic application security configuration based on users and roles

---
+++ src/main/java/augusto108/ces/springrestfulserv/config/ApplicationWebSecurityConfig.java
@@ -0,0 +1,67 @@
+package augusto108.ces.springrestfulserv.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+@PropertySource("classpath:application.properties")
+public class ApplicationWebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${users.username.user1}")
+    private String user1;
+
+    @Value("${users.username.user2}")
+    private String user2;
+
+    @Value("${users.username.user3}")
+    private String user3;
+
+    @Value("${users.username.user4}")
+    private String user4;
+
+    @Value("${users.roles.role1}")
+    private String role1;
+
+    @Value("${users.roles.role2}")
+    private String role2;
+
+    @Value("${users.roles.role3}")
+    private String role3;
+
+    @Value("${users.password}")
+    private String password;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username(user1).password(password).roles(role1))
+                .withUser(users.username(user2).password(password).roles(role1, role2))
+                .withUser(users.username(user3).password(password).roles(role1))
+                .withUser(users.username(user4).password(password).roles(role1, role3));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/guests/**").hasRole(role1)
+                .antMatchers(HttpMethod.POST, "/guests/**").hasRole(role2)
+                .antMatchers(HttpMethod.PUT, "/guests/**").hasRole(role2)
+                .antMatchers(HttpMethod.DELETE, "/guests/**").hasRole(role3)
+                .antMatchers(HttpMethod.PATCH, "/guests/**").hasAnyRole(role2, role3)
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
---
+++ src/main/java/augusto108/ces/springrestfulserv/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package augusto108.ces.springrestfulserv.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}

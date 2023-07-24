81e5c13778bad91cae98fd2ad1a6e9c69c621249
Cesar Augusto <ces.augusto108@gmail.com>
2023-07-24 17:19:13 -0300

    Flyway database migration

---
+++ src/main/resources/db/migration/V1__Create_Table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `tb_guest` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `guest_address_city` varchar(255) DEFAULT NULL,
+  `guest_address_number` int DEFAULT NULL,
+  `guest_address_street` varchar(255) DEFAULT NULL,
+  `guest_email` varchar(255) DEFAULT NULL,
+  `guest_email_domain_name` varchar(255) DEFAULT NULL,
+  `guest_email_username` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `stay` varchar(255) DEFAULT NULL,
+  `guest_telephone` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);

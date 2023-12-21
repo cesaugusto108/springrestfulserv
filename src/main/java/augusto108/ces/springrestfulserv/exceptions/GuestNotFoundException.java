08bd6bedce7818d6e720344a1c6b39157ebec6c1
Cesar Augusto <cesaraugusto@skiff.com>
2023-12-21 16:18:01 -0300

    Refactored GuestNotFoundException

--- src/main/java/augusto108/ces/springrestfulserv/exceptions/GuestNotFoundException.java
+++ src/main/java/augusto108/ces/springrestfulserv/exceptions/GuestNotFoundException.java
@@ -1,6 +1,7 @@
 package augusto108.ces.springrestfulserv.exceptions;
 
 public class GuestNotFoundException extends RuntimeException {
+
     public GuestNotFoundException(String message) {
         super(message);
     }

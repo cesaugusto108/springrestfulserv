c280f6707895a24942bea144b70e03e991b700c1
Cesar Augusto <cesaraugusto@skiff.com>
2023-12-21 19:20:24 -0300

    GuestRepository class refactoring

--- src/main/java/augusto108/ces/springrestfulserv/repositories/GuestRepository.java
+++ src/main/java/augusto108/ces/springrestfulserv/repositories/GuestRepository.java
@@ -1,7 +1,7 @@
 package augusto108.ces.springrestfulserv.repositories;
 
-import augusto108.ces.springrestfulserv.model.entities.Guest;
 import augusto108.ces.springrestfulserv.model.datatypes.Name;
+import augusto108.ces.springrestfulserv.model.entities.Guest;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,16 +9,14 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface GuestRepository extends CrudRepository<Guest, Long> {
+
     List<Guest> findByName(Name name);
 
-    @Query(
-            "from Guest g where " +
-                    "lower(first_name) like lower(concat('%', :search, '%')) or " +
-                    "lower(last_name) like lower(concat('%', :search, '%')) or " +
-                    "lower(guest_telephone) like lower(concat('%', :search, '%')) or " +
-                    "lower(guest_email) like lower(concat('%', :search, '%')) or " +
-                    "lower(stay) like lower(concat('%', :search, '%'))"
-    )
+    @Query("from Guest g where " +
+            "lower(g.name.firstName) like lower(concat('%', :search, '%')) or " +
+            "lower(g.name.lastName) like lower(concat('%', :search, '%')) or " +
+            "lower(g.telephone) like lower(concat('%', :search, '%')) or " +
+            "lower(g.email) like lower(concat('%', :search, '%')) or " +
+            "lower(g.stay) like lower(concat('%', :search, '%'))")
     List<Guest> searchGuests(@Param("search") String search);
-
 }
